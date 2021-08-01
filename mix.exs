b830164ec693ddac4c50ecb01d2a11694a895e06
João Thallis <joaothallis@icloud.com>
2021-08-01 03:37:19 -0300

    Add Dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule GoodreadsTwitter.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -22,7 +25,8 @@ defmodule GoodreadsTwitter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
